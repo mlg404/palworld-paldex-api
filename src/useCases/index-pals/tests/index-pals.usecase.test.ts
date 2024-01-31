0e80c672d912db776d9f9f017ee721cdb76aa5a8
Linzera <italolino@gmail.com>
2024-01-31 20:40:10 -0300

    Add tests for IndexPalsUseCase

---
+++ src/useCases/index-pals/tests/index-pals.usecase.test.ts
@@ -0,0 +1,89 @@
+import { describe, expect, test } from "bun:test";
+
+import { SuitabilitiesEnum } from "../../../common/enums";
+import { execute as getPalPage } from "../index-pals.usecase";
+
+describe("IndexPalsUseCase", () => {
+  test("should return a page of pals", async () => {
+    const result = getPalPage({});
+    expect(result).toMatchObject({
+      content: expect.any(Array),
+      total: expect.any(Number),
+      page: 1,
+      limit: 10,
+    });
+  });
+
+  test("should return a page of pals with a limit", () => {
+    const result = getPalPage({ limit: 5 });
+    expect(result).toMatchObject({
+      content: expect.any(Array),
+      total: expect.any(Number),
+      page: 1,
+      limit: 5,
+    });
+  });
+
+  test("should return correct page given page prop", () => {
+    const result = getPalPage({ page: 2 });
+    expect(result).toMatchObject({
+      content: expect.any(Array),
+      total: expect.any(Number),
+      page: 2,
+      limit: 10,
+    });
+  });
+
+  test("should filter pals by exact name", () => {
+    const palName = "Relaxaurus";
+
+    const result = getPalPage({
+      filter: {
+        name: palName,
+      },
+    });
+
+    expect(result.content).toHaveLength(1);
+  });
+
+  // TODO: Enhance search by type
+  // The suitabilities key does not exist on the JSON
+  test.skip("should filter pals by suitability", () => {
+    const palSuitability = SuitabilitiesEnum.ELECTRICITY;
+
+    const result = getPalPage({
+      filter: {
+        // This key does not exist on JSON
+        suitabilities: palSuitability,
+      },
+    });
+
+    expect(result.content).toHaveLength(2);
+  });
+
+  // TODO: Enhance search by partial name
+  test.skip("should filter pals by partial name", () => {
+    const palPartialName = "Jolt";
+
+    const result = getPalPage({
+      filter: {
+        name: palPartialName,
+      },
+    });
+
+    expect(result.content).toHaveLength(2);
+  });
+
+  // TODO: Enhance search by partial description
+  test.skip("should filter pals by partial description", () => {
+    const palDescription = "When thrown at an enemy";
+
+    const result = getPalPage({
+      filter: {
+        description: palDescription,
+      },
+    });
+
+    expect(result.content).toHaveLength(1);
+  });
+});
